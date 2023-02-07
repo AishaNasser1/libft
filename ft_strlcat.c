04dcca99b35190798d87319a70ddc0248a67988f
Aisha Nasser <anasser@lab1r3s3.42abudhabi.ae>
2023-02-07 19:35:15 +0400

    d

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: anasser <anasser@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 13:49:37 by anasser           #+#    #+#             */
-/*   Updated: 2023/02/07 18:09:06 by anasser          ###   ########.fr       */
+/*   Updated: 2023/02/07 19:30:48 by anasser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	if (size == 0)
 		return (ft_strlen(src));
 	else if (size < ft_strlen(dest))
-		value = ft_strlen(src) + size;
+		return (ft_strlen(src) + size);
 	else
 		value = ft_strlen(src) + ft_strlen(dest);
 	idx = 0;
@@ -39,17 +39,28 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	dest[idx] = '\0';
 	return (value);
 }
-/* 
-#include <stdio.h>
 
-int	main(void)
- {
-	char src[20] = "Hello World!";
-	char dst[20] = "Goodbye ";
-	size_t size = 15;
-	printf("Original destination string: %s\n", dst);
-	size_t len = ft_strlcat(dst, src, size);
-	printf("After strlcat: %s\n", dst);
-	printf("Length of final string: %lu\n", len);
-	return (0);
-} */
+// #include <stdio.h>
+// #include <string.h>
+// int	main(void)
+//  {
+// 	char src[30] = "lorem ipsum dolor sit amet";
+// 	char dst[20] = "01234567890123456790";
+
+// 	char src2[30] = "lorem ipsum dolor sit amet";
+// 	char dst2[20] = "01234567890123456790";
+
+// 	size_t size = 15;
+// 	printf("Original destination string: %s\n", dst);
+// 	printf("Builtin - Original destination string: %s\n", dst2);
+
+// 	size_t len = ft_strlcat(dst, src, size);
+// 	size_t len2 = strlcat(dst2, src2, size);
+
+// 	printf("After strlcat: %s\n", dst);
+// 	printf("Builtin - After strlcat: %s\n", dst2);
+
+// 	printf("Length of final string: %lu\n", len);
+// 	printf("Builtin - Length of final string: %lu\n", len2);
+// 	return (0);
+// }
